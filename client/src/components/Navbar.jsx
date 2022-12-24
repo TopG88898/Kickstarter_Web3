266e0b4436dc5e6800389a420be9e88d2bb499c2
TopG88898 <115095671+TopG88898@users.noreply.github.com>
2022-12-24 13:06:47 -0500

    init

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,16 @@ import { logo, menu, search, thirdweb } from '../assets'
 import { navlinks } from '../constants'
 import { useStateContext } from '../context'
 
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
+    <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#222228]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+        {!isActive ? (
+            <img src={imgUrl} className="w-1/2 h-1/2" />
+        ) : (
+            <img src={imgUrl} className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
+        )}
+    </div>
+)
+
 const Navbar = () => {
     const navigate = useNavigate();
     const [isActive, setIsActive] = useState('dashboard');
@@ -13,7 +23,7 @@ const Navbar = () => {
 
   return (
     <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
-        <div className='lg:flex-1 flex-row flex max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#16161a] rounded-[100px]'>
+        <div className='lg:flex-1 flex-row flex max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#16161a] rounded-[20px]'>
             <input type="text" placeholder='Search for campaigns' className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none' />
             <div className='w-[72px] h-full rounded-[20px] bg-[#9A9AEB] flex justify-center items-center cursor-pointer'>
                 <img src={search} className="w-[15px] h-[15px] object-contain" />
@@ -40,19 +50,21 @@ const Navbar = () => {
 
         <div className='sm:hidden flex justify-between relative items-center'>
             <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
-                <img src={logo} className="w-[60%] h-[60%] object-contain" />
+            <Link to="/">
+                <Icon styles="w-[52px] h-[52px] bg-[#222228]" imgUrl={logo} />
+            </Link>
             </div>
             <img 
                 src={menu} 
                 className="w-[22px] h-[22px] object-contain"
                 onClick={() => setToggleDrawer((prev) => !prev)}
             />
-            <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary rounded-[10px] py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+            <div className={`absolute top-[60px] right-0 left-0 bg-[#16161a] z-10 shadow-secondary rounded-[20px] py-6 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
                 <ul className='mb-4'> 
                     {navlinks.map((Link) => (
                         <li
                             key={Link.name}
-                            className={`flex p-4 ${isActive === Link.name && 'bg-[#3a3a43]'}`}
+                            className={`flex p-4 ${isActive === Link.name && 'bg-[#222228]'}`}
                             onClick={() => {
                                 setIsActive(Link.name);
                                 setToggleDrawer(false);
--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -40,12 +40,15 @@ const CampaignDetails = () => {
 
       <div className='w-full flex lg:flex-row flex-col mt-10 gap-[30px]'>
         <div className='flex-1 flex-col'>
-          <div className='flex justify-center'>
-            <img src={state.image} className="w-11/12 lg:h-[580px] md:h-[420px] h-[320px] object-cover rounded-xl" />
-          </div>
-          <div className='relative w-full h-[5px] bg-[#3a3a43] mt-2'>
-            <div className='absolute h-full bg-[#9A9AEB]' style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}}>
+          <div className='flex justify-center items-center flex-col'>
+            <div className='rounded-[20px] bg-[#16161a] mb-4 p-4 w-11/12'>
+              <h1 className='text-2xl font-semibold font-epilogue text-white'>{state.title}</h1>
             </div>
+            <img src={state.image} className="w-11/12 lg:h-[580px] md:h-[420px] h-[320px] object-cover rounded-xl" />
+            <div className='relative w-11/12 h-[5px] rounded-[20px] bg-[#3a3a43] mt-2'>
+            <div className='absolute h-full rounded-[20px] bg-[#9A9AEB]' style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}}>
+            </div>
+          </div>
           </div>
         </div>
         <div className='flex lg:w-[250px] w-full flex-wrap justify-between gap-[30px]'>
@@ -58,8 +61,8 @@ const CampaignDetails = () => {
         <div className='flex-[2] flex flex-col gap-[40px]'>
           <div>
             <h4 className='font-epilogue font-semibold text-white text-[18px] uppercase'>Creator</h4>
-            <div className='mt-[20px] flex flex-row items-center flex-wrap gap-[14px]'>
-              <div className='w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#16161a] cursor-pointer'>
+            <div className='mt-[20px] flex flex-row items-center flex-wrap gap-[14px] bg-[#16161a] p-4 rounded-[20px] max-w-[500px]'>
+              <div className='w-[52px] h-[52px] flex items-center justify-center rounded-full bg-black cursor-pointer'>
                 <img src={thirdweb} className="w-[60%] h-[60%] object-contein" />
               </div>
               <div>
@@ -71,12 +74,12 @@ const CampaignDetails = () => {
           <div>
             <h4 className='font-epilogue font-semibold text-white text-[18px] uppercase'>Story</h4>
             <div className='mt-[20px] max-w-[500px]'>
-              <p className='font-epilogue font-normal text-[14px] text-[#808191] leading-[26px] text-justify'>{state.description}</p>
+              <p className='font-epilogue font-normal text-[14px] text-[#808191] leading-[26px] text-justify bg-[#16161a] p-4 rounded-[20px]'>{state.description}</p>
             </div>
           </div>
           <div>
             <h4 className='font-epilogue font-semibold text-white text-[18px] uppercase'>Donators</h4>
-            <div className='mt-[20px] flex flex-col gap-4'>
+            <div className='mt-[20px] flex flex-col gap-4 max-w-[500px] bg-[#16161a] p-4 rounded-[20px]'>
               {donators.length > 0 ? donators.map((item, index) => (
                 <div key={`${item.donator}-${index}`} className="flex justify-between items-center gap-4">
                   <p className='font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-all'>{index + 1}. {item.donator}</p>
@@ -90,7 +93,7 @@ const CampaignDetails = () => {
         </div>
         <div className='flex-1'>
           <h4 className='font-epilogue font-semibold text-white text-[18px] uppercase'>Fund</h4>
-          <div className='mt-[20px] flex flex-col p-4 bg-[#16161a] rounded-[10px]'>
+          <div className='mt-[20px] flex flex-col p-5 bg-[#16161a] rounded-[20px]'>
               <p className='font-epilogue font-medium text-[20px] leading-[30px] text-center text-white'>Fund the campaign</p>
               <div className='mt-[30px]'>
                 <input 
@@ -101,7 +104,7 @@ const CampaignDetails = () => {
                   value={amount}
                   onChange={(e) => setAmount(e.target.value)}
                   />
-                  <div className='my-[20px] p-4 bg-[#16161a] rounded-[10px]'>
+                  <div className='my-[20px] p-2 bg-[#16161a] rounded-[20px]'>
                     <h4 className='font-epilogue font-semibold text-[14px] leading-[22px] text-white'>Back it because you believe in it.</h4>
                     <p className='mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]'>Support the project for no reward, just because it speaks to you.</p>
                   </div>
